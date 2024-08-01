6d82407211bde76523cc458c4684ff3f0f37d9aa
Jatin Mishra <99495405+itsjatinmishra@users.noreply.github.com>
2024-08-01 13:40:54 +0530

    first commit by itsjatinmishra

---
+++ itsjatinmishra/js/script.js
@@ -0,0 +1,86 @@
+//toggle icon navbar
+
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+};
+
+// scroll sections
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
+
+window.onscroll = () => {
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 100;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      // active navbar Links
+      navLinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector("header nav a[href*=" + id + "]")
+          .classList.add("active");
+      });
+      // active sections for animation on scroll
+      sec.classList.add("show-animate");
+    }
+    //if want to use animation that repeats on scroll use this
+    else {
+      sec.classList.remove("show-animate");
+    }
+  });
+  // sticky header
+  let header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 100);
+
+  //remove toggle icon and navbar when click navbar links (scroll)
+  menuIcon.classList.remove("bx-x");
+  navbar.classList.remove("active");
+
+  // animation footer on scroll
+  let footer = document.querySelector("footer");
+
+  footer.classList.toggle(
+    "show-animate",
+    this.innerHeight + this.scrollY >= document.scrollingElement.scrollHeight
+  );
+};
+
+
+const form = document.querySelector("form");
+
+async function SendMail() {
+  var params = {
+    from_name: document.getElementById("name").value,
+    email_Id: document.getElementById("email").value,
+    phone: document.getElementById("phone").value,
+    subject: document.getElementById("subject").value,
+    message: document.getElementById("message").value,
+  };
+
+  await emailjs.send("service_awvim2o", "template_wt06e7m", params)
+    .then(function (res) {
+      if (res.status === 200) {
+        Swal.fire({
+          title: "Success!",
+          text: "Message sent successfully!",
+          icon: "success",
+        });
+        form.reset();
+      }
+    })
+    .catch(function (error) {
+      Swal.fire({
+        title: "Error!",
+        text: "Something went wrong. Please try again.",
+        icon: "error",
+      });
+      console.log(error);
+    });
+}
